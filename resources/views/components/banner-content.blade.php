6629daffcbce3ef1d2ba343baeb050593912033e
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2024-11-14 19:25:47 +0700

    fix: z-index banner

--- resources/views/components/banner-content.blade.php
+++ resources/views/components/banner-content.blade.php
@@ -1,10 +1,10 @@
 @props(['title'])
 
 <div class="flex justify-between items-center w-full px-4 mb-10">
-	<div class="z-50 flex items-center gap-6">
+	<div class="z-[40] flex items-center gap-6">
 		<h1 class="text-[3rem] font-bold text-white">{{$title}}</h1>
 	</div>
-	<div class="flex flex-col items-end z-50">
+	<div class="flex flex-col items-end z-[10]">
 		<div class="bg-white flex items-center p-3 gap-5 rounded-md">
 			<img class="w-10 h-auto" src="{{ asset('/assets/icons/material-symbols_date-range.svg') }}" alt="calendar">
 			<div>
