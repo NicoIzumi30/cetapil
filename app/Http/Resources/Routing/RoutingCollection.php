4f60f1a7b7192866448a2252c4dcef1c2a8cbc0f
Enricko <Enricko.putra028@gmail.com>
2024-11-22 19:35:11 +0700

    refactor: update RoutingCollection to extend ResourceCollection and improve data transformation

--- app/Http/Resources/Routing/RoutingCollection.php
+++ app/Http/Resources/Routing/RoutingCollection.php
@@ -1,19 +1,24 @@
-<?php 
+<?php
+
 namespace App\Http\Resources\Routing;
 
 use App\Constants\RoutingConstants;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class RoutingCollection extends JsonResource 
+class RoutingCollection extends ResourceCollection
 {
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
     public function toArray(Request $request): array
-{
-   return [
-       "status" => "OK",
-       "message" => RoutingConstants::GET_LIST,
-       "data" => collect($this->resource)->except(['images', 'forms'])
-   ];
+    {
+        return [
+            "status" => "OK",
+            "message" => RoutingConstants::GET_LIST,
+            "data" => RoutingResource::collection($this->collection->except(['images', 'forms']))
+        ];
+    }
 }
-}
-?>
