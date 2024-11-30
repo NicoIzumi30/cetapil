9d946d0bafb27f7bffc2138b2ce6d1340a512077
Enricko <Enricko.putra028@gmail.com>
2024-11-30 22:30:14 +0700

    feat: include category in SellingResource response

--- app/Http/Resources/Selling/SellingResource.php
+++ app/Http/Resources/Selling/SellingResource.php
@@ -25,6 +25,7 @@ class SellingResource extends JsonResource
                     return [
                         'id' => $selling_product->id,
                         'product_id' => $selling_product->product_id,
+                        'category' => $selling_product->category,
                         'product_name' => $selling_product->product_name,
                         'stock' => $selling_product->stock,
                         'selling' => $selling_product->selling,
