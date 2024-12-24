4b21d418d1df07feb2d35c0c35180abc1be08afd
fadillarizky <fadillarizky294@gmail.com>
2024-12-24 11:12:20 +0700

    revisi form add visibility, and change api submit activity

---
+++ lib/model/list_product_sku_response.dart
@@ -0,0 +1,137 @@
+/// status : "OK"
+/// message : "Get the product list successfully."
+/// data : [{"id":"9dc8acdd-a584-4d48-a75c-695b668af227","sku":"CETAPHIL BABY DAILY LOTION 400 ML","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":146926},{"id":"9dc8acdd-bc45-4bba-acfd-c128dd38b051","sku":"Cetaphil Baby Daily Lotion with Organic Calendula 400 ml","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":167794},{"id":"9dc8acdd-d339-427a-8339-a01157ec8f1a","sku":"Cetaphil Baby Diaper Cream with Organic Calendula 70G","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":84444},{"id":"9dc8acdd-e9d1-46d0-b7d7-2e63e927b83a","sku":"CETAPHIL BABY GTL WASH & SHAMPOO 230 ML","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":83837},{"id":"9dc8acdd-ff79-4b1c-a9cf-8f3e1cd40bdd","sku":"CETAPHIL BABY GTL WASH & SHAMPOO 400 ML","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":122734},{"id":"9dc8acde-15fb-4ab1-9ac2-bbfba6fb972d","sku":"CETAPHIL BABY MOIST BATH & WASH 230 ML","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":94609},{"id":"9dc8acde-2c4f-4e9f-9d97-4e10001b05e7","sku":"Cetaphil Baby Moist Bath & Wash 400ml Pump","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":156221},{"id":"9dc8acde-431e-43a7-9087-b64414477eeb","sku":"CETAPHIL BABY SHAMPOO 200 ML","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":75636},{"id":"9dc8acde-5abe-41f3-847d-5acfbcbc6528","sku":"Cetaphil Baby Wash & Shampoo with Organic Calendula 230 ml","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":95473},{"id":"9dc8acde-7145-49bc-af9c-8fb37432e51e","sku":"Cetaphil Baby Wash & Shampoo with Organic Calendula 400 ml","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":138280},{"id":"9dc8acde-877f-4df8-9a39-405c833f21eb","sku":"Cetaphil Baby with Organic Calendula Advanced Protection Cream","category":{"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"},"price":71464},{"id":"9dc8acde-b3f9-4307-88f8-787853fe9dc0","sku":"Cetaphil Bright Healthy Radiance Gentle Renewing Cleanser 100","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":138566},{"id":"9dc8acde-cafe-48eb-b5da-86e7ab9f07b6","sku":"CETAPHIL Bright Healthy Radiance Perfecting Serum 30ml","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":287790},{"id":"9dc8acde-e15b-432a-b6ca-6c42718ed38d","sku":"CETAPHIL BRIGHT PR SHEET MASK 6X23 ML","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":197190},{"id":"9dc8acde-f884-4a00-be50-70bcaa97100b","sku":"Cetaphil Brightening Body Lotion 245ml","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":207429},{"id":"9dc8acdf-0f9f-422b-9432-1a1db77897bf","sku":"Cetaphil Brightening Day Protection Cream SPF 50ml","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":186686},{"id":"9dc8acdf-26a6-4aab-be6d-77fff3a92453","sku":"Cetaphil Brightening Night Nourishing Cream 50ml","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":193600},{"id":"9dc8acdf-3c7e-49e4-9de2-cb66861798db","sku":"Cetaphil Brightness Refresh Toner 150ml","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":145200},{"id":"9dc8acdf-52a2-4974-a017-95f64d217755","sku":"Cetaphil Brightness Reveal Creamy Cleanser 100ml","category":{"id":"9dc8acde-9e24-49b4-9bcc-c356744656e1","name":"BHR"},"price":138286},{"id":"9dc8acdf-7e9b-4031-800d-b19cf43b185d","sku":"CETAPHIL FACIAL CLEANSER 236ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":144493},{"id":"9dc8acdf-93f3-45e8-a7a9-75ff61c8583e","sku":"CETAPHIL Exfoliating Cleanser 178 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":81689},{"id":"9dc8acdf-a929-479b-9204-303defc2c44d","sku":"CETAPHIL GENTLE SKIN CLEANSER 1000 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":273216},{"id":"9dc8acdf-be8c-453f-876f-b752aae41fe7","sku":"CETAPHIL GENTLE SKIN CLEANSER 125 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":62095},{"id":"9dc8acdf-d410-459e-ba62-9370d4e5146e","sku":"CETAPHIL GENTLE SKIN CLEANSER 250 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":109392},{"id":"9dc8acdf-e952-4f6c-ba1b-4cbc90f977b1","sku":"CETAPHIL GENTLE SKIN CLEANSER 500 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":172905},{"id":"9dc8ace0-0017-4a7e-8911-8d3794f05de6","sku":"CETAPHIL GENTLE SKIN CLEANSER 59 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":37775},{"id":"9dc8ace0-15a8-4405-9b49-483b69ddd0bd","sku":"CETAPHIL HYDRATING FOAMING CREAM CLEANSER 236 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":112126},{"id":"9dc8ace0-2aa8-463c-b16f-9d1ada4eaef1","sku":"CETAPHIL OILY SKIN CLEANSER 125 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":149266},{"id":"9dc8ace0-402a-46c0-82e4-04cd9c37ff36","sku":"CETAPHIL SOOTHING FOAM WASH 2600ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":216486},{"id":"9dc8ace0-556f-4fc9-aa26-7c490de8ec7b","sku":"CETAPHIL ULTRA GENTLE BODY WASH 500 ML","category":{"id":"9dc8acdf-67ef-454a-9ef1-314aa1fc013d","name":"Cleanser"},"price":81689},{"id":"9dc8ace0-8207-466c-9dde-d1cfaa14600f","sku":"CETAPHIL DAILY ADV ULT HYDRA LOTION 85 G","category":{"id":"9dc8ace0-6b0f-421f-b787-a49992e3a0a6","name":"Moisturizer"},"price":95878},{"id":"9dc8ace0-97bf-4c57-b77f-35ecf98c1fca","sku":"CETAPHIL MOISTURISING LOTION 237 mL","category":{"id":"9dc8ace0-6b0f-421f-b787-a49992e3a0a6","name":"Moisturizer"},"price":144493},{"id":"9dc8ace0-ae72-4918-92a3-b3d15dcd366f","sku":"CETAPHIL MOISTURIZING CREAM 453 G","category":{"id":"9dc8ace0-6b0f-421f-b787-a49992e3a0a6","name":"Moisturizer"},"price":242500},{"id":"9dc8ace0-c56d-4ba6-ad5d-dcf487ea879d","sku":"CETAPHIL MOISTURIZING CREAM - 100 G","category":{"id":"9dc8ace0-6b0f-421f-b787-a49992e3a0a6","name":"Moisturizer"},"price":112770},{"id":"9dc8ace0-db19-41a9-98fd-c4966b903d94","sku":"CETAPHIL MOISTURIZING LOTION 200 ML","category":{"id":"9dc8ace0-6b0f-421f-b787-a49992e3a0a6","name":"Moisturizer"},"price":132703},{"id":"9dc8ace0-f0cb-4a17-af01-e2165bf38101","sku":"CETAPHIL MOISTURIZING LOTION 59 ML","category":{"id":"9dc8ace0-6b0f-421f-b787-a49992e3a0a6","name":"Moisturizer"},"price":48254},{"id":"9dc8ace1-1b71-4f15-bb9e-88b282bc1651","sku":"CETAPHIL DAILY FACIAL MOIST SPF15 118 ML","category":{"id":"9dc8ace1-0621-4afd-927c-9864c0b401f9","name":"Sun Protect"},"price":153495},{"id":"9dc8ace1-30a7-42f5-8ebd-3182afbb55ec","sku":"CETAPHIL UVA/UVB DEF SPF 50+/PA+33 50 ML","category":{"id":"9dc8ace1-0621-4afd-927c-9864c0b401f9","name":"Sun Protect"},"price":195541},{"id":"9dc8ace1-460b-4eaa-aedf-b2015e7483c9","sku":"CETAPHIL Sun SPF50+ Light Gel 50ml","category":{"id":"9dc8ace1-0621-4afd-927c-9864c0b401f9","name":"Sun Protect"},"price":244964},{"id":"9dc8ace1-5b48-45c3-a75e-a822c6d2fd8e","sku":"CETAPHIL UVA/UVB MOIST 255 ML","category":{"id":"9dc8ace1-0621-4afd-927c-9864c0b401f9","name":"Sun Protect"},"price":279583},{"id":"9dc8ace1-85df-4634-946e-97b4b96019f2","sku":"CETAPHIL PRO AD DERMA REPAIR CREAM 227 G","category":{"id":"9dc8ace1-70b7-4ab3-9855-2d94202a7108","name":"Pro AD"},"price":287684},{"id":"9dc8ace1-9bea-4885-acfe-f048a701a448","sku":"CETAPHIL PRO AD DERMA SKIN RESTORING MOISTURIZER 145 ML","category":{"id":"9dc8ace1-70b7-4ab3-9855-2d94202a7108","name":"Pro AD"},"price":164573},{"id":"9dc8ace1-b0db-4736-9ba8-b8541cf7ee18","sku":"CETAPHIL PRO AD DERMA WASH 295 ML","category":{"id":"9dc8ace1-70b7-4ab3-9855-2d94202a7108","name":"Pro AD"},"price":246041}]
+
+class ListProductSkuResponse {
+  ListProductSkuResponse({
+      String? status, 
+      String? message, 
+      List<Data>? data,}){
+    _status = status;
+    _message = message;
+    _data = data;
+}
+
+  ListProductSkuResponse.fromJson(dynamic json) {
+    _status = json['status'];
+    _message = json['message'];
+    if (json['data'] != null) {
+      _data = [];
+      json['data'].forEach((v) {
+        _data?.add(Data.fromJson(v));
+      });
+    }
+  }
+  String? _status;
+  String? _message;
+  List<Data>? _data;
+ListProductSkuResponse copyWith({  String? status,
+  String? message,
+  List<Data>? data,
+}) => ListProductSkuResponse(  status: status ?? _status,
+  message: message ?? _message,
+  data: data ?? _data,
+);
+  String? get status => _status;
+  String? get message => _message;
+  List<Data>? get data => _data;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = _status;
+    map['message'] = _message;
+    if (_data != null) {
+      map['data'] = _data?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
+
+/// id : "9dc8acdd-a584-4d48-a75c-695b668af227"
+/// sku : "CETAPHIL BABY DAILY LOTION 400 ML"
+/// category : {"id":"9dc8acdd-8f67-48a0-a097-b8405719c50c","name":"Baby"}
+/// price : 146926
+
+class Data {
+  Data({
+      String? id, 
+      String? sku, 
+      Category? category, 
+      num? price,}){
+    _id = id;
+    _sku = sku;
+    _category = category;
+    _price = price;
+}
+
+  Data.fromJson(dynamic json) {
+    _id = json['id'];
+    _sku = json['sku'];
+    _category = json['category'] != null ? Category.fromJson(json['category']) : null;
+    _price = json['price'];
+  }
+  String? _id;
+  String? _sku;
+  Category? _category;
+  num? _price;
+Data copyWith({  String? id,
+  String? sku,
+  Category? category,
+  num? price,
+}) => Data(  id: id ?? _id,
+  sku: sku ?? _sku,
+  category: category ?? _category,
+  price: price ?? _price,
+);
+  String? get id => _id;
+  String? get sku => _sku;
+  Category? get category => _category;
+  num? get price => _price;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = _id;
+    map['sku'] = _sku;
+    if (_category != null) {
+      map['category'] = _category?.toJson();
+    }
+    map['price'] = _price;
+    return map;
+  }
+
+}
+
+/// id : "9dc8acdd-8f67-48a0-a097-b8405719c50c"
+/// name : "Baby"
+
+class Category {
+  Category({
+      String? id, 
+      String? name,}){
+    _id = id;
+    _name = name;
+}
+
+  Category.fromJson(dynamic json) {
+    _id = json['id'];
+    _name = json['name'];
+  }
+  String? _id;
+  String? _name;
+Category copyWith({  String? id,
+  String? name,
+}) => Category(  id: id ?? _id,
+  name: name ?? _name,
+);
+  String? get id => _id;
+  String? get name => _name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = _id;
+    map['name'] = _name;
+    return map;
+  }
+
+}
