cf608ba7ff10d1e40cc33fd10329853eeb6aed46
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2025-01-04 16:02:54 +0700

    minor: delete Memuat Data text when processing datatable

--- resources/views/pages/routing/request.blade.php
+++ resources/views/pages/routing/request.blade.php
@@ -70,7 +70,6 @@
                 dom: 'rt<"bottom-container"<"bottom-left"l><"bottom-right"p>>',
                 language: {
                     lengthMenu: "Menampilkan _MENU_ dari _TOTAL_ data",
-                    processing: "Memuat data...",
                     paginate: {
                         previous: '<',
                         next: '>',
