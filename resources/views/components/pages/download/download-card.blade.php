d91ff8f25d8d5205b337e037816141856b472b9c
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2025-01-07 19:16:42 +0700

    style: add fix height download card

--- resources/views/components/pages/download/download-card.blade.php
+++ resources/views/components/pages/download/download-card.blade.php
@@ -1,6 +1,6 @@
 @props(['cardTitle','cardFooter', 'iconName'])
 
-	<div class="w-full rounded-lg bg-glassmorphism p-6">
+	<div class="w-full rounded-lg bg-glassmorphism p-6 h-fit">
 		<div class="flex gap-6 w-full items-center justify-center mb-6">
 			<img src="{{ asset("/assets/icons/{$iconName}.svg") }}" class="w-[40px] h-[40px]" alt="{{$cardTitle}} icon">
 			<h1 class="font-bold text-3xl text-white uppercase">
